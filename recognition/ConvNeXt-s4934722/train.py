1de101da66100599364a85ad06c25010b371e1fe
Thanh Nhat Quang Mai <thanhnhatquang.mai@student.uq.edu.au>
2025-10-11 16:34:54 +1000

    Add validate function for model evaluation with loss, accuracy, and metric computation

--- recognition/ConvNeXt-s4934722/train.py
+++ recognition/ConvNeXt-s4934722/train.py
@@ -39,5 +39,37 @@ def train_one_epoch(model, dataloader, criterion, optimizer, epoch):
         total += batch_size
 
     avg_loss = total_loss / total
-    accuracy = correct / total
+    accuracy = 100 * correct / total
     return avg_loss, accuracy
+
+def validate(model, dataloader, criterion, epoch):
+    model.eval()
+    total_loss, correct, total = 0.0, 0, 0
+    all_preds, all_labels = [], []
+
+    with torch.inference_mode():
+        for images, labels in tqdm(dataloader, desc=f"Epoch {epoch} [Val]"):
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            batch_size = images.size(0)
+            total_loss += loss.item() * batch_size
+            total_loss += loss.item() * batch_size
+            _, predicted = outputs.max(1)
+            correct += predicted.eq(labels).sum().item()
+            total += batch_size
+
+            all_preds.extend(predicted.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+    avg_loss = total_loss / loss
+    accuracy = 100 * correct / total
+
+    # Compute additional metrics
+    precision = precision_score(y_true=all_labels, y_pred=all_preds)
+    recall = recall_score(y_true=all_labels, y_pred=all_preds)
+    f1 = f1_score(y_true=all_labels, y_pred=all_preds)
+    cm = confusion_matrix(y_true=all_labels, y_pred=all_preds)
+
+    return avg_loss, accuracy, precision, recall, f1, cm
